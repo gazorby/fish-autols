35e5397272ac2d6b2440fe62bcde0373bdfef84b
Gazorby <gazorby@pm.me>
2020-04-20 11:39:33 +0200

    ✨ feat: auto detect exa

--- conf.d/auto_exa.fish
+++ conf.d/auto_exa.fish
@@ -5,10 +5,14 @@ function __auto_exa_hook --description "Auto exa" --on-event fish_prompt
 
   if test "$__auto_exa_last" != (pwd)
     echo
-    if test -d "$PWD"/.git
-      exa --long --header --git
+    if type -q exa
+      if test -d "$PWD"/.git
+        exa --long --header --git
+      else
+        exa -la
+      end
     else
-      exa -la
+      ls -la
     end
   end
   set  -g __auto_exa_last (pwd)
